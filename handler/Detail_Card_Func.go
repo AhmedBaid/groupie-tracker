7d5af0cd42525386d182599db4596a524cefdd74
ahmed <ahmed@baid.com>
2025-02-24 13:56:25 +0000

    groupit-tracker last version

--- handler/Detail_Card_Func.go
+++ handler/Detail_Card_Func.go
@@ -1,7 +1,7 @@
 package handler
 
 import (
-	"encoding/json"
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -16,29 +16,13 @@ func Detail_Card_Func(w http.ResponseWriter, r *http.Request) {
 		helpers.RenderTemplates(w, "statusPage.html", tools.ErrorMethodnotAll, http.StatusMethodNotAllowed)
 		return
 	}
-	// fetching the artists
-	var fetch_artist []tools.Artists
-	url := "https://groupietrackers.herokuapp.com/api/artists"
-	// get the api data
-	res, err := http.Get(url)
-	if err != nil {
-		helpers.RenderTemplates(w, "statusPage.html", tools.ErrorInternalServerErr, http.StatusInternalServerError)
-		return
-	}
-	defer res.Body.Close()
-	// decode the jsone data
-	err = json.NewDecoder(res.Body).Decode(&fetch_artist)
-	if err != nil {
-		helpers.RenderTemplates(w, "statusPage.html", tools.ErrorInternalServerErr, http.StatusInternalServerError)
-		return
-	}
 	type fetchingData struct {
 		Artist    *tools.Artists
 		Locations *tools.Locations
 		Dates     *tools.ConcertDates
 		Relations *tools.Relations
 	}
-	// get the id from url
+	// Get the id from the url
 	id := r.URL.Query().Get("id")
 	// to int
 	Id, err := strconv.Atoi(id)
@@ -47,15 +31,8 @@ func Detail_Card_Func(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	var artistFound *tools.Artists
-	// get the user
-	for i := range fetch_artist {
-		if Id == fetch_artist[i].Id {
-			artistFound = &fetch_artist[i]
-			break
-		}
-	}
-	//  to see if the user exists
-	if artistFound == nil {
+	errFetch := helpers.Fetch_By_Id(fmt.Sprintf("https://groupietrackers.herokuapp.com/api/artists/%d", Id), &artistFound)
+	if artistFound.Id == 0 {
 		helpers.RenderTemplates(w, "statusPage.html", tools.ErrorNotFound, http.StatusNotFound)
 		return
 	}
@@ -66,26 +43,17 @@ func Detail_Card_Func(w http.ResponseWriter, r *http.Request) {
 	Locations_url := artistFound.Locations
 	ConcertDates_url := artistFound.ConcertDates
 	Relations_url := artistFound.Relations
-	
-	// fetch the location and get the result  in the location variable
-	errr := helpers.Fetch_By_Id(Locations_url, &locations)
-	if errr != nil {
+
+	// fetch the nested data
+	errLoc := helpers.Fetch_By_Id(Locations_url, &locations)
+	errLates := helpers.Fetch_By_Id(ConcertDates_url, &dates)
+	errRelation := helpers.Fetch_By_Id(Relations_url, &relations)
+
+	if errLoc != nil || errLates != nil || errRelation != nil || errFetch != nil {
 		helpers.RenderTemplates(w, "statusPage.html", tools.ErrorInternalServerErr, http.StatusInternalServerError)
 		return
 	}
-	// fetch the dates and get the result  in the dates variable
-	errr = helpers.Fetch_By_Id(ConcertDates_url, &dates)
-	if errr != nil {
-		helpers.RenderTemplates(w, "statusPage.html", tools.ErrorInternalServerErr, http.StatusInternalServerError)
-		return
-	}
-	// fetch the relations and get the result  in the relations variable
-	errr = helpers.Fetch_By_Id(Relations_url, &relations)
-	if errr != nil {
-		helpers.RenderTemplates(w, "statusPage.html", tools.ErrorInternalServerErr, http.StatusInternalServerError)
-		return
-	}
-	// set all the data  that we found into the fetching variable
+	// set all the data that we fetched into one variable
 	fetching_data := fetchingData{
 		Artist:    artistFound,
 		Locations: locations,
